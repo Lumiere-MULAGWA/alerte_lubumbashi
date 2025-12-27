1da4aff567fe4f999022251ddd2c0db757248717
lumiere-MULAGWA <21mb358@esisalama.org>
2025-12-27 14:11:05 +0200

    add a database engine

---
+++ backend/app/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+
+
+from app.core.config import settings
+
+
+engine: Engine = create_engine(
+settings.DATABASE_URL,
+echo=False,
+pool_pre_ping=True
+)
+
+
+SessionLocal = sessionmaker(
+autocommit=False,
+autoflush=False,
+bind=engine
+)
+
+
+
+
+def get_db():
+db = SessionLocal()
+try:
+yield db
+finally:
+db.close()
