be446f2daafc88b76fd8321e40e2f4ecec97d1f7
lumiere-MULAGWA <21mb358@esisalama.org>
2025-12-27 14:36:52 +0200

    add a auth.py

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+
+
+class RegisterSchema(BaseModel):
+full_name: str
+email: EmailStr
+password: str
+
+
+class LoginSchema(BaseModel):
+email: EmailStr
+password: str
+
+
+class TokenSchema(BaseModel):
+access_token: str
+token_type: str = "bearer"
