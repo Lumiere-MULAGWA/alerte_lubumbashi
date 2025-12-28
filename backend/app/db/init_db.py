ddef30eb29abc02331689b1c3d4e45c44c796081
lumiere-MULAGWA <21mb358@esisalama.org>
2025-12-28 12:59:51 +0200

    modification de fonction7

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -6,7 +6,7 @@ from app.models.base import Base
 
 
 def init_db():
-with engine.begin() as conn:
-# Activer PostGIS
-conn.execute(text("CREATE EXTENSION IF NOT EXISTS postgis"))
-Base.metadata.create_all(bind=conn)
+    with engine.begin() as conn:
+    # Activer PostGIS
+        conn.execute(text("CREATE EXTENSION IF NOT EXISTS postgis"))
+        Base.metadata.create_all(bind=conn)
