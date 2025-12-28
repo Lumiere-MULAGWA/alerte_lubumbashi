523ba72d1805e083c0e0478b2f37223404bd3364
lumiere-MULAGWA <21mb358@esisalama.org>
2025-12-28 13:02:17 +0200

    modification de fonction7

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -8,37 +8,37 @@ from app.repositories.user_repository import UserRepository
 class AuthService:
 
 
-def __init__(self, repo: UserRepository):
-self.repo = repo
+    def __init__(self, repo: UserRepository):
+        self.repo = repo
 
 
-def register(self, full_name: str, email: str, password: str):
-existing = self.repo.get_by_email(email)
-try:
-can_register_user(existing is not None)
-except ValueError:
-raise HTTPException(status_code=400, detail="Email déjà utilisé")
+    def register(self, full_name: str, email: str, password: str):
+        existing = self.repo.get_by_email(email)
+        try:
+            can_register_user(existing is not None)
+        except ValueError:
+            raise HTTPException(status_code=400, detail="Email déjà utilisé")
 
 
-user = User(
-full_name=full_name,
-email=email,
-password_hash=hash_password(password)
-)
-self.repo.create(user)
+        user = User(
+        full_name=full_name,
+        email=email,
+        password_hash=hash_password(password)
+        )
+        self.repo.create(user)
 
 
-token = create_access_token({"sub": str(user.id), "role": user.role})
-return token
+        token = create_access_token({"sub": str(user.id), "role": user.role})
+        return token
 
 
-def login(self, email: str, password: str):
-user = self.repo.get_by_email(email)
-try:
-validate_login(user is not None, verify_password(password, user.password_hash) if user else False)
-except ValueError:
-raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Identifiants invalides")
+    def login(self, email: str, password: str):
+        user = self.repo.get_by_email(email)
+        try:
+            validate_login(user is not None, verify_password(password, user.password_hash) if user else False)
+        except ValueError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Identifiants invalides")
 
 
-token = create_access_token({"sub": str(user.id), "role": user.role})
-return token
+        token = create_access_token({"sub": str(user.id), "role": user.role})
+        return token
